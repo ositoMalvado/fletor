0e3683c191f8465821960f7f2f61275281573aab
Julián Perez <66093948+ositoMalvado@users.noreply.github.com>
2024-06-12 23:11:51 -0300

    + Types

--- fletor/__init__.py
+++ fletor/__init__.py
@@ -2,5 +2,13 @@ from .ResponsiveControl import ResponsiveControl
 from .ResponsiveRow import ResponsiveRow
 from .ResponsiveColumn import ResponsiveColumn
 from .Stepper import Stepper
-from .types import StepperStyle
-__all__ = ["ResponsiveControl", "ResponsiveRow", "ResponsiveColumn", "Stepper", "StepperStyle"]
+from .types import *
+__all__ = [
+    "ResponsiveControl",
+    "ResponsiveRow",
+    "ResponsiveColumn",
+    "Stepper",
+    "StepperStyle"
+    "StepperOrientation",
+    "StepperStepsPosition",
+]
--- fletor/types.py
+++ fletor/types.py
@@ -1,9 +1,18 @@
-
+from enum import Enum
 from typing import Dict, Tuple, Union
 
-class StepperStyle():
-    fill = "fill"
-    one = "one"
-    fill_end = "fill_end"
-    fill_start = "fill_start"
-    fill_between = "fill_between"
+class StepperStyle(Enum):
+    FILL = "fill"
+    ONE = "one"
+    FILL_END = "fill_end"
+    FILL_START = "fill_start"
+    FILL_BETWEEN = "fill_between"
+
+
+class StepperOrientation(Enum):
+    HORIZONTAL = "horizontal"
+    VERTICAL = "vertical"
+
+class StepperStepsPosition(Enum):
+    START = "start"
+    END = "end"
